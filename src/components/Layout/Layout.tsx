59ec8c5117f300fbf8cee3677ea10c24b64c08ab
nehalist <git@nehalist.io>
2022-12-14 21:01:22 +0100

    fix: fix theme toggle

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -70,9 +70,9 @@ export const Layout: FunctionComponent<{ children: ReactNode }> = ({
           <label className="inline-flex relative items-center cursor-pointer">
             <input
               type="checkbox"
-              onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+              onClick={() => setTheme(theme !== "dark" ? "dark" : "light")}
               className="sr-only peer"
-              checked={theme === "light"}
+              defaultChecked={theme === "light"}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 flex items-center justify-between px-1 text-base">
               <MdLightMode />
