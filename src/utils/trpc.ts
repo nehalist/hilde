93ae830bd1f40162056afa136ed5c5c03b4bc2c4
nehalist <git@nehalist.io>
2022-12-20 22:12:15 +0100

    feat: disable ssr

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -19,18 +19,5 @@ export const trpc = createTRPCNext<AppRouter>({
       ],
     };
   },
-  ssr: true,
-  responseMeta({ clientErrors }) {
-    if (clientErrors.length) {
-      // propagate first http error from API calls
-      return {
-        status: clientErrors[0].data?.httpStatus ?? 500,
-      };
-    }
-    // cache full page for 1 day + revalidate once every second
-    const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-    return {
-      "Cache-Control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-    };
-  },
+  ssr: false,
 });
