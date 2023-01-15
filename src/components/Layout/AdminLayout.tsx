8363d9126b86ad0b3e76358b45003b6b224e6d7e
nehalist <git@nehalist.io>
2023-01-15 14:12:54 +0100

    feat: improve admin ui

--- src/components/Layout/AdminLayout.tsx
+++ src/components/Layout/AdminLayout.tsx
@@ -1,10 +1,18 @@
 import { ReactNode } from "react";
+import { signOut } from "next-auth/react";
 
 export const AdminLayout = ({ children }: { children: ReactNode }) => (
   <div>
-    <nav className="inline">
+    <nav className="border-b mb-2 pb-2 border-gray-600 text-right text-sm font-semibold">
       <ul>
-        <li>Seasons</li>
+        <li>
+          <button
+            className="py-1 px-2 dark:bg-gray-600 bg-gray-200 rounded"
+            onClick={() => signOut({ callbackUrl: "/" })}
+          >
+            Logout
+          </button>
+        </li>
       </ul>
     </nav>
     <div>{children}</div>
