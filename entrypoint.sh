111511b1030346d5a458934f2dd88804ffef8811
nehalist <git@nehalist.io>
2023-01-15 14:46:32 +0100

    build: fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-test -n "$APP_NEXT_PUBLIC_SEASON"
+test -n "$APP_ADMIN_PASSWORD"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SEASON#$NEXT_PUBLIC_SEASON#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_ADMIN_PASSWORD#$ADMIN_PASSWORD#g"
 npx prisma migrate deploy
 node server.js
