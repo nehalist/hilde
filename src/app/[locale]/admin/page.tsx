617e377ae46a8d8a79dcecabf3b95f3f63be1694
nehalist <git@nehalist.io>
2024-02-06 22:08:35 +0100

    refactor: simplify admin

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,72 +1,30 @@
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "@/lib/navigation";
-import { db } from "@/db";
-import { users } from "@/db/schema";
-import { count, gt, sql } from "drizzle-orm";
-import { eachDayOfInterval, isEqual } from "date-fns";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
-import { AdminDashboardChart } from "@/app/[locale]/admin/chart";
+import { db } from "@/db";
+import { leagues, matches, memberships, teams, users } from "@/db/schema";
+import { eq, sql } from "drizzle-orm";
 
-export async function getDashboardData() {
-  const recentUsers = await db
-    .select({
-      createdAt: sql<string>`date_trunc
-      ('day',
-      ${users.createdAt}
-      )`,
-      count: sql<number>`cast(count(
-      ${users.id}
-      )
-      as
-      int
-      )`,
-    })
-    .from(users)
-    .where(
-      gt(
-        users.createdAt,
-        sql`NOW
-        () - INTERVAL '30 days'`,
-      ),
-    )
-    .groupBy(
-      sql`date_trunc
-      ('day',
-      ${users.createdAt}
-      )`,
-    );
-  const [totalUsers] = await db.select({ count: count() }).from(users);
-
-  const course = eachDayOfInterval({
-    start: new Date(new Date().setDate(new Date().getDate() - 30)),
-    end: new Date(),
-  })
-    .reverse()
-    .reduce<{
-      days: Array<{ date: string; count: number }>;
-      remaining: number;
-    }>(
-      (acc, day) => {
-        const countOnDay =
-          recentUsers.find(u => isEqual(new Date(u.createdAt), day))?.count ||
-          0;
-
-        return {
-          days: [
-            ...acc.days,
-            {
-              date: day.toISOString(),
-              count: acc.remaining - countOnDay,
-            },
-          ],
-          remaining: acc.remaining - countOnDay,
-        };
-      },
-      { days: [], remaining: totalUsers.count },
-    );
-
+async function getStats() {
   return {
-    userCourse: course.days.reverse(),
+    leagues: {
+      active: await db
+        .select({ count: sql<number>`count(*)::int` })
+        .from(leagues)
+        .where(eq(leagues.status, "active")),
+      finished: await db
+        .select({ count: sql<number>`count(*)::int` })
+        .from(leagues)
+        .where(eq(leagues.status, "finished")),
+    },
+    memberships: await db
+      .select({ count: sql<number>`count(*)::int` })
+      .from(memberships),
+    users: await db.select({ count: sql<number>`count(*)::int` }).from(users),
+    teams: await db.select({ count: sql<number>`count(*)::int` }).from(teams),
+    matches: await db
+      .select({ count: sql<number>`count(*)::int` })
+      .from(matches),
   };
 }
 
@@ -75,27 +33,34 @@ export default async function Admin() {
   if (user?.role !== "admin") {
     return redirect("/");
   }
-  const data = await getDashboardData();
+  const stats = await getStats();
 
   return (
-    <div className="flex flex-col gap-3">
-      <Card>
-        <CardBody>
-          <AdminDashboardChart data={data} />
-        </CardBody>
-      </Card>
-      <div className="grid grid-cols-3 gap-3">
+    <div>
+      <p>It's a very poor admin, but it's a start.</p>
+      <div className="grid grid-cols-3 gap-3 mt-3">
         <Card>
           <CardHeader>Total users</CardHeader>
-          <CardBody>...</CardBody>
+          <CardBody>{stats.users[0].count}</CardBody>
         </Card>
         <Card>
           <CardHeader>Total teams</CardHeader>
-          <CardBody>...</CardBody>
+          <CardBody>{stats.teams[0].count}</CardBody>
+        </Card>
+        <Card>
+          <CardHeader>Total memberships</CardHeader>
+          <CardBody>{stats.memberships[0].count}</CardBody>
+        </Card>
+        <Card>
+          <CardHeader>Total leagues</CardHeader>
+          <CardBody>
+            {stats.leagues.active[0].count} active,{" "}
+            {stats.leagues.finished[0].count} finished
+          </CardBody>
         </Card>
         <Card>
           <CardHeader>Total matches</CardHeader>
-          <CardBody>...</CardBody>
+          <CardBody>{stats.matches[0].count}</CardBody>
         </Card>
       </div>
     </div>
